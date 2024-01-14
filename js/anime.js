946fdd01f20c6a7bb67c218f115f1a07e64a3268
marco-soria <marco.soria@pucp.edu.pe>
2024-01-13 21:22:09 -0500

    carousel

--- js/anime.js
+++ js/anime.js
@@ -149,8 +149,13 @@ carruseles.forEach((carrusel) => {
     });
 });
 
+/*burguer menu */
+let mainNav = document.getElementById("js-menu");
+let navBarToggle = document.getElementById("js-navbar-toggle");
 
-
+navBarToggle.addEventListener("click", function() {
+  mainNav.classList.toggle("active");
+});
 
 
 
